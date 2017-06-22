2ec06decf5970189ae788ceab8fbdc36e4738b62
Alex P. Andrade <g.alex.andrade@gmail.com>
2017-06-21 23:47:17 -0300

    Commit

--- src/main/java/com/teamtreehouse/core/WebSecurityConfiguration.java
+++ src/main/java/com/teamtreehouse/core/WebSecurityConfiguration.java
@@ -1,15 +1,19 @@
 package com.teamtreehouse.core;
 
 
+import com.teamtreehouse.security.JWTAuthenticationFilter;
+import com.teamtreehouse.security.JWTLoginFilter;
 import com.teamtreehouse.user.DetailsService;
 import com.teamtreehouse.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,18 +24,38 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("123")
+                .roles("ADMIN");
+
+        /*
         auth.userDetailsService(userDetailsService)
-            .passwordEncoder(User.PASSWORD_ENCODER);
+            .passwordEncoder(User.PASSWORD_ENCODER); */
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        /*
         http.authorizeRequests()
-                .antMatchers("/api/v1/courses").permitAll()
+                .antMatchers("/api/v1/authentication").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic()
                 .and()
                 .csrf().disable();
+                */
+
+        http.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/v1/authentication").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                // We filter the api/login requests
+                .addFilterBefore(new JWTLoginFilter("/api/v1/authentication", authenticationManager()),
+                        UsernamePasswordAuthenticationFilter.class)
+                // And filter other requests to check the presence of JWT in header
+                .addFilterBefore(new JWTAuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class);
     }
 }
