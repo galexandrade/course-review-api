c5518cb30ff26bddd7abb2becf0704d67704b5b3
Alex P. Andrade <g.alex.andrade@gmail.com>
2017-06-21 21:40:50 -0300

    Commit

--- src/main/java/com/teamtreehouse/core/WebSecurityConfiguration.java
+++ src/main/java/com/teamtreehouse/core/WebSecurityConfiguration.java
@@ -27,6 +27,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/api/v1/courses").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic()
